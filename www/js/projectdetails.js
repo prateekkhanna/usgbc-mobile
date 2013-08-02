06a77453e57f678ea35457d3191626ce5a14e119
Prateek Khanna <pkhanna@pkmini.local>
2013-08-02 18:33:17 -0400

    projects

---
+++ www/js/projectdetails.js
@@ -0,0 +1,24 @@
+$( document ).on( "pageshow", "#detailsPage", function(event) {
+	var id = getUrlVars()["id"];
+	var serviceurl = 'http://www.usgbc.org/mobile/services/projects/' + id;
+	$.getJSON(serviceurl, displayProject);
+});
+
+
+
+function displayProject(data) {
+	console.log(data);
+	var projects = data.nodes;
+	$.each(projects, function(index, project) {	
+		project = project.node;
+		$('#projectPic').attr('src',project.image2);
+		$('#name').text(project.name);
+		$('#address').html(project.address);
+		$('#foundation_statement').html(project.foundation_statement);
+		$('#description').html(project.description);		
+		$('#city').text(project.city);
+	});
+
+	
+}
+
---
+++ www/js/projectlist.js
@@ -0,0 +1,86 @@
+var page = getUrlVars()["page"];
+if(!page) page=0;
+var scrollheight = -500;
+var serviceURL = "http://www.usgbc.org/mobile/services/projects";
+serviceURL = serviceURL + "?page=" + page;
+serviceURL = serviceURL + "?callback=";
+var projects;
+
+$( document ).on( "click", ".ui-input-clear", function() {
+	getProjectList();
+});
+
+$('#projectListPage').bind('pageinit', function(event) {
+	getProjectList();
+	$( "#projectList" ).on( "listviewbeforefilter", function ( e, data ) {	
+		var $ul = $( this ),
+			$input = $( data.input ),
+			value = $input.val(),
+			html = "";
+			$ul.html( "" );
+		if ( value && value.length > 2 ) {
+			getProjectList(0,$input.val());
+		}
+	});
+});
+
+function getProjectList(pagenum, search) {
+	if(!pagenum) pagenum=0;
+	serviceURL = "http://www.usgbc.org/mobile/services/projects";
+	serviceURL = serviceURL + "?page=" + pagenum;
+	
+	if(search != null){
+		serviceURL = "http://www.usgbc.org/mobile/services/projects";
+		serviceURL = serviceURL + "?search=" + search + "&page=" + pagenum;
+	}
+	serviceURL = serviceURL + "?callback=";
+		
+	console.log(serviceURL);
+	$.getJSON(serviceURL, function(data) {
+		console.log(data);
+		if(pagenum<1){
+			$('#projectList li').remove();
+			$('#projectList #load-more').remove();				
+		} else {
+			$('#projectList #load-more').remove();			
+		}
+		projects = data.nodes;
+		$.each(projects, function(index, project) {
+			$('#projectList').append('<li><a href="projectdetails.html?id=' + project.node.id + '">' +
+					'<img style="max-width:100px;max-height:80px" src="' + project.node.image + '"/>' +
+					'<p style="margin:10px;margin-bottom:0px;margin-top:0px"><strong>' + project.node.name  + '</strong></p>' +
+					'<p style="margin:10px;margin-top:0px;margin-bottom:0px;">' + project.node.rating_system + ' ' + project.node.version + '</p>' +
+					'<div style="position:absolute;right:5px;font-size:10px;top:2px">' + project.node.certification_level + '</div>' + 
+					'<p class="ui-li-desc" style="margin:10px;margin-top:5px;color:#666">' + project.node.city + ', ' + project.node.state +  '</p></a></li>');
+		});
+		$('#projectList').append('<li id="load-more"><center>Loading...</center></li>');		
+		$('#projectList').listview('refresh');
+		$('#projectList').waypoint(function(direction){
+			if(direction == "down"){
+				page = page + 1;
+				scrollheight = scrollheight * (page+1);
+				if(search == null) getProjectList(page); 
+				if(search != null) 	$('#projectList #load-more').remove();	
+			}
+			console.log("hello world" + direction);
+		}, { offset: function() {
+			    return -$('#projectList').height() + 1000;
+			  } });
+
+	});
+}
+						
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+
